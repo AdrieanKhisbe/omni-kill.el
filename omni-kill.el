547b0a1ccc3a82223fe3072308e834207d296021
Adriean Khisbe <adriean.khisbe@live.fr>
2014-09-06 15:39:07 +0200

    init: stub file

---
+++ omni-kill.el
@@ -0,0 +1,33 @@
+;;; omni-kill.el --- Kill them all the things
+
+;; Copyright (C) 2014  Adrien Becchis
+
+;; Author: Adrien Becchis <adriean.khisbe@live.fr>
+;; Created:  2014-09-06
+;; Version: 0.1
+;; Keywords: convenience, languages, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; `ok' is the namespace for internal function that are not part of the
+;; public api of the mode.
+
+;;; Code:
+
+
+
+(provide 'omni-kill)
+;;; omni-kill.el ends here

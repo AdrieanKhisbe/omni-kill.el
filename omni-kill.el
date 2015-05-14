345dcc46be8ee8752f91c1c9e10cddac3c6efff3
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-14 10:12:23 +0200

    refactor: apply conventions and kill ok:. update copydate + Commentary section

--- omni-kill.el
+++ omni-kill.el
@@ -1,10 +1,10 @@
 ;;; omni-kill.el --- Kill them all the things
 
-;; Copyright (C) 2014  Adrien Becchis
+;; Copyright (C) 2014-2015  Adrien Becchis
 
 ;; Author: Adrien Becchis <adriean.khisbe@live.fr>
 ;; Created:  2014-09-06
-;; Version: 0.2
+;; Version: 0.2.1
 ;; Keywords: convenience, editing, tools
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -22,8 +22,11 @@
 
 ;;; Commentary:
 
-;; `ok' is the namespace for internal function that are not part of the
-;; public api of the mode.
+;; `omni-kill' is there to assist you as emacs user to quickly copy,
+;; select, kill, ... all the things that are at point such as emails,
+;; url, sentence...
+;;
+;; It's built on top of `thingatpt', and just provide wrapper functions.
 
 ;;; Code:
 
@@ -35,7 +38,7 @@
 
 (defcustom omni-kill-thing-list
   '(symbol list sexp defun filename url email word sentence whitespace line number page )
-  "List of THING symbols for which omni kill will create a fonction"
+  "List of THING symbols for which omni kill will create a function."
   :type '(repeat symbol)
   :group 'omni-kill)
 
@@ -51,13 +54,13 @@ Changing this would only have effect at next startup."
   :type '(restricted-sexp
           :match-alternatives
           ((lambda (x) (and (stringp x)
-			    (string-match-p "\\s_*%s\\s_*%s\\s_*" x)
-			    ;; ¤note: hack since did not managed to make the anchors work
-			    (not (string-match-p "\\s-" x))))))
+                            (string-match-p "\\s_*%s\\s_*%s\\s_*" x)
+                            ;; ¤note: hack since did not managed to make the anchors work
+                            (not (string-match-p "\\s-" x))))))
   :group 'omni-kill)
 ;; §name: maybe copy-this-THING rather than omny-copy by default
 
-(defcustom ok:thing-to-letter-alist
+(defcustom omni-kill-thing-to-letter-alist
   '((?d . defun)
     (?e . email)
     (?f . filename)
@@ -71,7 +74,7 @@ Changing this would only have effect at next startup."
     (?u . url)
     (?W . whitespace)
     (?w . word))
-  "Alist to store the letter associated with a thing for the vi like functions"
+  "Alist to store the letter associated with a thing for the vi like functions."
   :type '(alist :key-type char :value-type symbol))
 
 ;; ¤> thing at point wrappers
@@ -79,30 +82,30 @@ Changing this would only have effect at next startup."
 
 ;; kill-thing at point
 
-(defun ok:kill-thing-at-point (thing)
+(defun omni-kill-kill-thing-at-point (thing)
   "Kill the THING at point if any.
 
 Returns nil."
   (let ((frontier (bounds-of-thing-at-point thing) ))
     ;; §later: try catch error?
     (if frontier
-	 (kill-region (car frontier) (cdr frontier))
-	(message "There is not a %s at point!" thing))
+         (kill-region (car frontier) (cdr frontier))
+        (message "There is not a %s at point!" thing))
     ;; §check if can be chained.
     nil))
 
-(defun ok:delete-thing-at-point (thing)
+(defun omni-kill-delete-thing-at-point (thing)
   "Delete the THING at point if any.
 
 Returns nil."
   (let ((frontier (bounds-of-thing-at-point thing) ))
     ;; §later: try catch error?
     (if frontier
-	 (delete-region (car frontier) (cdr frontier))
-	(message "There is not a %s at point!" thing))
+         (delete-region (car frontier) (cdr frontier))
+        (message "There is not a %s at point!" thing))
     nil))
 
-(defun ok:select-thing-at-point (thing)
+(defun omni-kill-select-thing-at-point (thing)
   ;; ¤note: similar to mark...
   "Select the THING at point if any.
 
@@ -110,109 +113,107 @@ Returns nil."
   (let ((frontier (bounds-of-thing-at-point thing) ))
     ;; §later: try catch error?
     (if frontier
-	(progn
-	  (push-mark) ; save old mark
-	  (set-mark (car frontier))
-	  (goto-char  (cdr frontier)))
-	(message "There is not a %s at point!" thing))
+        (progn
+          (push-mark) ; save old mark
+          (set-mark (car frontier))
+          (goto-char  (cdr frontier)))
+        (message "There is not a %s at point!" thing))
     nil))
 ;; §maybe: message should go up, in thegenerated functions (note: after macro extraction?)
 
 
-(defun ok:copy-thing-at-point (thing)
-  "Try to copy the THING at point.  (use kill-new for now)
+(defun omni-kill-copy-thing-at-point (thing)
+  "Try to copy the THING at point.  (use `kill-new' for now).
 
 Returns the value grabed, otherwise nil."
   ;; §later: reconsider signature? (send the catch?)
   (let ((the-thing (thing-at-point thing) ))
     ;; §later: try catch error?
     (if the-thing
-	(progn
-	 (kill-new the-thing)
-	 (message "%s was copied" thing)
-	 the-thing)
+        (progn
+         (kill-new the-thing)
+         (message "%s was copied" thing)
+         the-thing)
       (progn
-	(message "There is not a %s at point!" thing)
-	     nil))))
+        (message "There is not a %s at point!" thing)
+             nil))))
 
 ;; §later:  make it in the clipboard.
 ;; §see: clipboard function: clipboard-yank, etc!!!!!
 ;; §see: xsel
 
-;; §later: store-thing..
+;; §later: store-thing! :)
 
 ;;; ¤> Function generators
-(defun ok:generate-all-the-fun (thing)
-  "Generate all the functions associated with the given THING"
-  (ok:generate-copy-command thing)
-  (ok:generate-delete-command thing)
-  (ok:generate-kill-command thing)
-  (ok:generate-select-command thing)
-  (mapc (lambda (a) (ok:generate-dispatch-command a))
-	'("copy" "delete" "kill" "select")))
+(defun omni-kill-generate-all-the-fun (thing)
+  "Generate all the functions associated with the given THING."
+  (omni-kill-generate-copy-command thing)
+  (omni-kill-generate-delete-command thing)
+  (omni-kill-generate-kill-command thing)
+  (omni-kill-generate-select-command thing)
+  (mapc (lambda (a) (omni-kill-generate-dispatch-command a))
+        '("copy" "delete" "kill" "select")))
 ;; §later: factorize macros + §next bump: two level multiplexer!!! on action, then on thing!!
 
-(defun omni-help ()
-  "Display the letter to thing associations for the omni-dispatch functions"
+(defun omni-kill-help ()
+  "Display the letter to thing associations for the omni-dispatch functions."
   (interactive)
   (message "%s%s"
-	   (propertize "Letter2Thing: " 'face 'font-lock-type-face)
-	   (mapconcat (lambda (cs) (format "%c:%s" (car cs) (cdr cs)))
-		      ok:thing-to-letter-alist " ")))
+           (propertize "Letter2Thing: " 'face 'font-lock-type-face)
+           (mapconcat (lambda (cs) (format "%c:%s" (car cs) (cdr cs)))
+                      omni-kill-thing-to-letter-alist " ")))
 
-(defmacro ok:generate-dispatch-command (action)
-  "Generate a dispath command for the given action."
+(defmacro omni-kill-generate-dispatch-command (action)
+  "Generate a dispath command for the given ACTION."
  `(defun ,(intern (format "omni-%s" (eval action))) (char)
        ,(format "%s the thing associated with the given CHAR.
-Association are stored in the `ok:thing-to-letter-alist' variable" (capitalize (eval action))) ;§todo: doc
+Association are stored in the `omni-kill-thing-to-letter-alist' variable" (capitalize (eval action))) ;§todo: doc
        (interactive "cPick a thing:");§later: recap list
-       (let ((thing (cdr-safe (assoc char ok:thing-to-letter-alist))))
-	 (if thing
-	     (,(intern (format "ok:%s-thing-at-point" (eval action) )) thing)
-	     (message "No thing is associated at letter %s  (for memory refresh, run `omni-help')" char)
-	   nil))))
+       (let ((thing (cdr-safe (assoc char omni-kill-thing-to-letter-alist))))
+         (if thing
+             (,(intern (format "omni-kill-%s-thing-at-point" (eval action) )) thing)
+             (message "No thing is associated at letter %s  (for memory refresh, run `omni-help')" char)
+           nil))))
 
 ;; §maybe: macro generate the macro... ^^
-(defmacro ok:generate-copy-command (symb)
+(defmacro omni-kill-generate-copy-command (symb)
   "Generate a copy command for the given SYMB."
  `(defun ,(intern (format omni-kill-naming-scheme "copy" (eval symb))) ()
        ,(format "Copy the %s at point" (eval symb))
        (interactive)
-       (ok:copy-thing-at-point ',(eval symb))))
+       (omni-kill-copy-thing-at-point ',(eval symb))))
 
-(defmacro ok:generate-delete-command (symb)
+(defmacro omni-kill-generate-delete-command (symb)
   "Generate a delete command for the given SYMB."
   `(defun ,(intern (format omni-kill-naming-scheme "delete" (eval symb))) ()
        ,(format "Delete the %s at point"  (eval symb))
        (interactive)
-       (ok:delete-thing-at-point ',(eval symb))))
+       (omni-kill-delete-thing-at-point ',(eval symb))))
 
-(defmacro ok:generate-kill-command ( symb)
+(defmacro omni-kill-generate-kill-command ( symb)
   "Generate a kill command for the given SYMB."
   `(defun ,(intern (format omni-kill-naming-scheme "kill" (eval symb))) ()
      ,(format "Kill the %s at point"  (eval symb))
      (interactive)
-     (ok:kill-thing-at-point ',(eval symb))))
+     (omni-kill-kill-thing-at-point ',(eval symb))))
 
-(defmacro ok:generate-select-command ( symb)
+(defmacro omni-kill-generate-select-command ( symb)
   "Generate a select command for the given SYMB."
   `(defun ,(intern (format omni-kill-naming-scheme "select" (eval symb))) ()
      ,(format "Select the %s at point"  (eval symb))
      (interactive)
-     (ok:select-thing-at-point ',(eval symb))))
+     (omni-kill-select-thing-at-point ',(eval symb))))
 
-(defun ok:get-all-the-things()
+(defun omni-kill-get-all-the-things()
   "Generate all the omni functions for the list of things."
   ;; §tofix: eager macro expansion failure
-  ;; §wtf: does not happ when manually load.
-  (mapc (lambda (arg) (ok:generate-all-the-fun arg))
-	omni-kill-thing-list))
-;; §maybe user custom for list?
-
-
+  ;; §wtf: does not happen when manually load.
+  (mapc (lambda (arg) (omni-kill-generate-all-the-fun arg))
+        omni-kill-thing-list))
+;; §maybe: user custom for list?
 
 ;; set up all commands:
-(ok:get-all-the-things)
+(omni-kill-get-all-the-things)
 
 (provide 'omni-kill)
 ;;; omni-kill.el ends here

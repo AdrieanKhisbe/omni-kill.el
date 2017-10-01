2a345a887a08e6cffc3d50638a38d59140696a8b
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-10-01 21:51:02 +0200

    doc: updated README

--- omni-kill.el
+++ omni-kill.el
@@ -22,8 +22,8 @@
 
 ;;; Commentary:
 
-;; `omni-kill' is there to assist you as emacs user to quickly copy,
-;; select, kill, ... all the things that are at point such as emails,
+;; `omni-kill' is there to assist you as emacs user to quickly kill,
+;; select, copy, ... all the things that are at point such as emails,
 ;; url, sentence...
 ;;
 ;; It's built on top of `thingatpt', and just provide wrapper functions.
